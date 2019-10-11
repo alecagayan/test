822d6dc6026ca9ab0aa405cf519552b00c6cac3e
oopsie1412 <55986805+oopsie1412@users.noreply.github.com>
2019-10-11 14:49:44 -0400

    update and rename alcebot.py
    basically it now detects words inside a message so i could say "epic badass santa" and it would detect the part that says "badass santa"

--- alcebot.py
+++ alcebot.py
@@ -1,27 +1,29 @@
-
 import discord
+import asyncio
+import re
 
 class MyClient(discord.Client):
-
     async def on_ready(self):
+    
         await client.change_presence(status=discord.Status.do_not_disturb, activity=discord.Game('with your emotions'))
-
         print('Logged in as:')
         print(self.user.name)
         print(self.user.id)
         print('------')
-        
+
     async def on_message(self, message):
 
-        if message.content.startswith('cookie'):
+        if 'cookie' in message.content:
             await message.channel.send(':cookie:'.format(message))
-            
-        if message.content.startswith('poopoo'):
+
+        if 'poopoo' in message.content:
             await message.channel.send(':poop:'.format(message))
 
-        if message.content.startswith('badass santa'):
+        if 'badass santa' in message.content:
             await message.channel.send(':santa: :gun:'.format(message))
-            
-client = MyClient()
 
-client.run('NDgwNDUxNDM5MTgxOTU1MDkz.XZ-ddg.pGaejxojdGg3rJt6rkYhN5TYMIg')
+        if 'test' in message.content:
+            await message.channel.send(':beer:'.format(message))
+
+client = MyClient()
+client.run('NDgwNDUxNDM5MTgxOTU1MDkz.XaCDQg.wvCQm2H-K-5E41-0MJuEQYn6vEc')
